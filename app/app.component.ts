3568322d29e0287ec62bbe6609400bd42d20bd11
RobertTSLee <robertlee5877@gmail.com>
2016-11-27 17:58:37 +0800

    feat(app.component): html template

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,14 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <h1>Hello {{name}}</h1>
-    <h2>Experiment conducted by RobertTSLee and weijyewang</h2>
-    <h4>Stay tuned for more!</h4>
-    <h4>Excitement Level: {{excitementLevel}} %</h4>
-  `,
+  templateUrl: 'app/app.component.html'
 })
 export class AppComponent  { 
+  class='absolute';
   name = 'Angular'; 
   excitementLevel = 100;
 }
